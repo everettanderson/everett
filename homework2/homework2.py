f48f404572c8fb6a1cf7bad68dfa9d6a98e2d814
everettanderson <everett_anderson@berkeley.edu>
2025-09-11 22:58:45 -0700

    homework 2

---
+++ homework2/homework2.py
@@ -0,0 +1,59 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+# Git is what's on your computer, GitHub is the collection of pushed Gits
+# for everyone, and Git Bash is the shell used to open it.
+# 2) What’s the difference between the terminal and the command line?
+#The terminal is where a command line is located, and the command line
+#is where inputs are read.
+# 3) How does Windows PowerShell differ from Git Bash?
+#Each uses its own language.
+# 4) What’s the difference between Anaconda, conda, and Python?
+#Python is the language used, conda is the package, and anaconda is the
+#distro.
+# 5) What is VS Code? 
+#vs code is a code editor which relies on a GUI
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+#A Jupyter Notebook is a computing environment, and is simpler/easier
+#than lab.
+# 7) What does ~/ mean?
+#It is a path to the current user home directory.
+# 8) What’s the difference between an absolute path and a relative path?
+#An absolute path starts at the top of the directory, and relative is
+#from the current user location.
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+#/users/evdan/python_decal_fa25/everett, ~/
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd - or cd ..
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# removes the current directory
+# 12) What do the following commands do?
+# git add
+# git commit
+# git push
+#git add puts it in the staging area, commit saves locally, and push 
+#sends to github
+# 13)What's the difference between "git add ." and "git add <file>"?
+#. is for the current directory, the file picks one.
+# 14) What do "git status" and "git log -1" do?
+# status checks what's on the branch/uncommitted, log lists what's pushed.
+# 15) What’s the difference between cloning a repository and pulling from it?
+#cloning makes a copy at home, pulling also brings changes across.
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+#I set up an ssh key but didn't notice I put a password on it, so I couldn't
+#connect to ssh and had to reset it.
+# 17) What’s a question you still have? What’s something you’re confused about?
+#How does pull vs clone work? If they're both bringing the most up-to-date
+#version to the local machine.
+# 18) Tell me a fun fact!
+#Sunfish are barely hunted because they taste so bad, so they have no
+#real natural predators.
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print("bool(True and False)")
+# booleans are interesting as to what it considers as "something" for true.
---
+++ lecture_notes/test.py
@@ -0,0 +1,7 @@
+# File test.py
+
+print(5+5)
+print(10-2)
+print(3*9)
+print(12/3)
+print(6**2)
